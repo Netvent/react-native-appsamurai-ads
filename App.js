5646fee904ad064734656a2883606e54fd4fd8ff
Olcay Ay <olcayay@gmail.com>
2019-07-01 12:04:32 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,81 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {NativeModules} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  state = { 
+    log: ""
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>React Native Bridge Test App!</Text>
+        <Button
+          onPress={this.onPress}
+          title="Press Me"
+          color="#841584"
+        />
+        <Text style={styles.instructions}>{this.state.log}</Text>
+      </View>
+    );
+  }
+
+  onPress = () => {
+    var platform = "Unknown"
+    if ( Platform.OS === 'android' ) {
+      platform = 'Android'
+    } else if (Platform.OS === 'ios') {
+      platform = 'iOS'
+    }
+    this.setLog(`OS type is ${platform}`);
+    this.showMessage()
+  }
+
+  setLog(log){
+    this.setState({
+      log: log
+    })
+  }
+
+  showMessage = () => {
+    if ( Platform.OS === 'android' ) {
+      NativeModules.ToastExample.show('Awesome', NativeModules.ToastExample.SHORT);
+    } else if (Platform.OS === 'ios') {
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
